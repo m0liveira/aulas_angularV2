513ef1259b12433ab7152361e85722c455a9989e
Mateus Oliveira <mateusamaraloliveira160302@gmail.com>
2021-04-26 12:31:22 +0100

    hamburguer menu

--- project/src/app/components/nav-bar/nav-bar.component.ts
+++ project/src/app/components/nav-bar/nav-bar.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  menuOpen: boolean = false;
+  burguer: any;
+
+  openMenu(): void {
+    if (!this.menuOpen) {
+      this.menuOpen = true;
+    } else {
+      this.menuOpen = false;
+    }
   }
-
 }
