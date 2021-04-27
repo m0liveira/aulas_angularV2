3a6c6ef03e8364a2573cb3faee43997b57b2a335
Mateus Oliveira <mateusamaraloliveira160302@gmail.com>
2021-04-27 10:31:31 +0100

    nav fix

--- project/src/app/components/nav-bar/nav-bar.component.ts
+++ project/src/app/components/nav-bar/nav-bar.component.ts
@@ -6,33 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent implements OnInit {
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 
   menuOpen: boolean = false;
 
+  // when burguer pressed
   openMenu(burguer: HTMLElement, overlay: HTMLElement, refUl: HTMLElement): void {
     if (!this.menuOpen) {
       burguer.classList.toggle('open');
-      overlay.style.transform = "translateX(0)";
-      overlay.style.background = "#000014d9";
-      refUl.style.transform = "translateX(0)";
+      overlay.style.transform = 'translateX(0)';
+      overlay.style.background = '#000014d9';
+      refUl.style.transform = 'translateX(0)';
       this.menuOpen = true;
     } else {
       burguer.classList.toggle('open');
-      overlay.style.background = "transparent";
-      refUl.style.transform = "translateX(-100%)";
+      overlay.style.background = 'transparent';
+      refUl.style.transform = 'translateX(-110%)';
       this.menuOpen = false;
 
       setTimeout(() => {
-        overlay.style.transform = "translateX(-100%)";
+        overlay.style.transform = 'translateX(-100%)';
       }, 500);
     }
   }
 
+  // when link pressed
   navChoice(burguer: HTMLElement, overlay: HTMLElement, refUl: HTMLElement): void {
     this.openMenu(burguer, overlay, refUl);
   }
-
 }
