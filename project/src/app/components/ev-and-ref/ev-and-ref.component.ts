6fdd7c9d626e94260c2491bc87516a6377d1c1ca
Mateus Oliveira <75847514+m0liveira@users.noreply.github.com>
2021-05-29 17:16:04 +0100

    evs and refs starting

--- project/src/app/components/ev-and-ref/ev-and-ref.component.ts
+++ project/src/app/components/ev-and-ref/ev-and-ref.component.ts
@@ -6,13 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ev-and-ref.component.css'],
 })
 export class EvAndRefComponent implements OnInit {
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   bg: string;
+  sith: boolean;
 
   changeColor(): void {
     this.bg = this.bg == 'red' ? 'white' : 'red';
+    this.bg = 'red';
+    console.log("mudou");
+  }
+
+  hideChooser(box: HTMLElement): void {
+    box.style.display = "none";
+    console.log(this.sith);
+  }
+
+  republicJedi(box: HTMLElement): void {
+    this.sith = false;
+    this.hideChooser(box);
+  }
+
+  sithJedi(box: HTMLElement): void {
+    this.sith = true;
+    this.hideChooser(box);
   }
 }
